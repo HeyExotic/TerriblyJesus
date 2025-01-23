806196cd5839a398f18deea6855a28ce88f8ad6c
HeyExotic <RubixKingdom3211@gmail.com>
2025-01-22 18:24:53 -0600

    App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,20 @@
 package org.example;
-
+import java.util.Scanner;
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
+   Scanner scanner = new Scanner(System.in);
+
+   System.out.println("Welcome to the Ounce Conversion Progam!");
+
+    System.out.println("How many ounces do you have?");
+    String input = scanner.nextLine().trim();
+
+    System.out.println("\nThank you! Converting Ounces to Pounds");
+    int ounces = Integer.parseInt(input);
+    System.out.println(Converter.toPounds(ounces));
+    System.out.println(Converter.toPoundsAndOunces(ounces));
+
+    System.out.println("\nThank you for using the OCP!");
+    scanner.close();
   }
 }
