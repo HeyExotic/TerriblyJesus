de0012c5f702a05030cc89fd7514efb7cf527a30
HeyExotic <RubixKingdom3211@gmail.com>
2025-01-22 18:24:33 -0600

    Converter.java

--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,5 +1,14 @@
 package org.example;
 
 public class Converter {
-  // Your code goes here
+  public static String toPounds(double ounces) {
+  double pounds = ounces / 16.0;
+    return String.format("%d oz is equivalent to %d lbs", (int) ounces, pounds);
+  }
+  public static String toPoundsAndOunces(double ounces) {
+      int pounds = (int) (ounces / 16);
+    int remainingounces = (int) (ounces % 16);
+    return String.format("%d oz is equivalent to %d lbs and %d oz", (int) ounces, pounds, remainingounces);
+  }
 }
+
